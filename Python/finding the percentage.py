1fafcad723d524f6e9a41b4159c69d9514e3151f
kaanthaekanayake <kaanthaekanayake.personal@gmail.com>
2025-10-21 06:56:03 +0530

    Explain the code using coments

--- Python/finding the percentage.py
+++ Python/finding the percentage.py
@@ -1,14 +1,17 @@
 n = int(input("Enter num of Students : "))
 
-records = {}
+records = {} # Dictionary to store name and the score list
 
+# read n number of lines with "name score1 score2 .."
 for i in range (n):
-    parts = input().split()
-    name = parts[0]
-    scores = list(map(float,parts[1:]))
-    records[name] = scores
+    parts = input().split() #split the line to tokens from the white spaces
+    name = parts[0] # First token is the student name
+    scores = list(map(float,parts[1:])) # rest of the list items are scores and convert them in to floats
+    records[name] = scores # store name and the scores as key:value pair in a dictionary
 
-query_name = input().strip()
-student_scores = records[query_name]
-average = sum(student_scores)/len(student_scores)
-print("{:.2f}".format(average))
+#find the average of the relavent name's score list
+query_name = input().strip()  # get the name and remove unnesssary white spaces and /n characters
+#input already with a /n- new line character it remove using strip
+student_scores = records[query_name] # get the score relavent to the given name as input
+average = sum(student_scores)/len(student_scores) # find the average
+print("{:.2f}".format(average)) # print the average with two decimal places
