9a7f278746ed13a9e0d1e537cdcd7ed93b5c6b40
kaanthaekanayake <kaanthaekanayake.personal@gmail.com>
2025-10-29 06:51:24 +0530

    solve Swap case

---
+++ Python/Swapcase.py
@@ -0,0 +1,17 @@
+# Convert lower case letters to uppser case vise versa.
+def swap_case(s):
+    swaped_string = [] # use to add letters after change its case
+
+    for L in s:
+        if L.islower(): # check whether the letter is lower case
+            swaped_string.append(L.upper()) # convert lower to upper
+        elif L.isupper(): # check whether the letter is upper case
+            swaped_string.append(L.lower()) # convert upper to lower
+        else:
+            swaped_string.append(L)
+    return "".join(swaped_string) # Join above array's letters to a single string
+
+if __name__ == '__main__':
+    s = input().strip()
+    result = swap_case(s)
+    print(result)
